3121f3782de1ba863e2e3464792997bfba034b27
oggi82 <57406801+oggi82@users.noreply.github.com>
2019-11-28 19:46:59 +0100

    ports/xmc: clang format

--- ports/xmc/mpconfigport.h
+++ ports/xmc/mpconfigport.h
@@ -1,53 +1,54 @@
-#include <stdint.h>
 #include "mpconfigboard.h"
+#include <stdint.h>
 // options to control how Micro Python is built
 
 //#define MICROPY_QSTR_EXTRA_POOL     mp_qstr_frozen_const_pool
-#define MICROPY_ALLOC_PATH_MAX      (128)
+#define MICROPY_ALLOC_PATH_MAX (128)
 #define MICROPY_ALLOC_PARSE_CHUNK_INIT (16)
-#define MICROPY_EMIT_THUMB          (1)
-#define MICROPY_EMIT_INLINE_THUMB   (1)
-#define MICROPY_COMP_MODULE_CONST   (1)
-#define MICROPY_COMP_CONST          (1)
+#define MICROPY_EMIT_THUMB (1)
+#define MICROPY_EMIT_INLINE_THUMB (1)
+#define MICROPY_COMP_MODULE_CONST (1)
+#define MICROPY_COMP_CONST (1)
 #define MICROPY_COMP_DOUBLE_TUPLE_ASSIGN (1)
 #define MICROPY_COMP_TRIPLE_TUPLE_ASSIGN (1)
-#define MICROPY_ENABLE_GC           (1)
-#define MICROPY_HELPER_REPL         (1)
-#define MICROPY_REPL_EMACS_KEYS     (1)
-#define MICROPY_REPL_AUTO_INDENT    (1)
-#define MICROPY_ENABLE_SOURCE_LINE  (1)
-#define MICROPY_ERROR_REPORTING     (MICROPY_ERROR_REPORTING_NORMAL)
-#define MICROPY_OPT_COMPUTED_GOTO   (1)
-#define MICROPY_OPT_MPZ_BITWISE     (1)
+#define MICROPY_ENABLE_GC (1)
+#define MICROPY_HELPER_REPL (1)
+#define MICROPY_REPL_EMACS_KEYS (1)
+#define MICROPY_REPL_AUTO_INDENT (1)
+#define MICROPY_ENABLE_SOURCE_LINE (1)
+#define MICROPY_ERROR_REPORTING (MICROPY_ERROR_REPORTING_NORMAL)
+#define MICROPY_OPT_COMPUTED_GOTO (1)
+#define MICROPY_OPT_MPZ_BITWISE (1)
 #define MICROPY_CAN_OVERRIDE_BUILTINS (1)
-#define MICROPY_USE_INTERNAL_ERRNO  (1)
+#define MICROPY_USE_INTERNAL_ERRNO (1)
+#define MICROPY_ENABLE_COMPILER (1)
 #define MICROPY_PY_BUILTINS_STR_UNICODE (1)
 #define MICROPY_PY_BUILTINS_BYTEARRAY (1)
 #define MICROPY_PY_BUILTINS_MEMORYVIEW (1)
 #define MICROPY_PY_BUILTINS_ENUMERATE (1)
-#define MICROPY_PY_BUILTINS_FILTER  (1)
+#define MICROPY_PY_BUILTINS_FILTER (1)
 #define MICROPY_PY_BUILTINS_FROZENSET (1)
 #define MICROPY_PY_BUILTINS_REVERSED (1)
-#define MICROPY_PY_BUILTINS_SET     (1)
-#define MICROPY_PY_BUILTINS_SLICE   (1)
+#define MICROPY_PY_BUILTINS_SET (1)
+#define MICROPY_PY_BUILTINS_SLICE (1)
 #define MICROPY_PY_BUILTINS_PROPERTY (1)
 #define MICROPY_PY_BUILTINS_MIN_MAX (1)
-#define MICROPY_PY_BUILTINS_HELP    (1)
+#define MICROPY_PY_BUILTINS_HELP (1)
 #define MICROPY_PY_BUILTINS_HELP_TEXT xmc_help_text
 #define MICROPY_PY_BUILTINS_HELP_MODULES (1)
-#define MICROPY_PY_GC               (1)
-#define MICROPY_PY_ARRAY            (1)
-#define MICROPY_PY_ATTRTUPLE        (1)
-#define MICROPY_PY_COLLECTIONS      (1)
-#define MICROPY_PY_MATH             (1)
-#define MICROPY_PY_CMATH            (1)
-#define MICROPY_PY_IO               (0)
-#define MICROPY_PY_STRUCT           (1)
-#define MICROPY_PY_SYS              (1)
-#define MICROPY_PY_SYS_STDFILES     (0)
-#define MICROPY_MODULE_FROZEN_MPY   (0)
-#define MICROPY_LONGINT_IMPL        (MICROPY_LONGINT_IMPL_MPZ)
-#define MICROPY_FLOAT_IMPL          (MICROPY_FLOAT_IMPL_FLOAT)
+#define MICROPY_PY_GC (1)
+#define MICROPY_PY_ARRAY (1)
+#define MICROPY_PY_ATTRTUPLE (1)
+#define MICROPY_PY_COLLECTIONS (1)
+#define MICROPY_PY_MATH (1)
+#define MICROPY_PY_CMATH (1)
+#define MICROPY_PY_IO (0)
+#define MICROPY_PY_STRUCT (1)
+#define MICROPY_PY_SYS (1)
+#define MICROPY_PY_SYS_STDFILES (0)
+#define MICROPY_MODULE_FROZEN_MPY (0)
+#define MICROPY_LONGINT_IMPL (MICROPY_LONGINT_IMPL_MPZ)
+#define MICROPY_FLOAT_IMPL (MICROPY_FLOAT_IMPL_FLOAT)
 
 // type definitions for the specific machine
 // Pin definition header file
@@ -55,7 +56,7 @@
 
 #define BYTES_PER_WORD (4)
 
-#define MICROPY_MAKE_POINTER_CALLABLE(p) ((void*)((mp_uint_t)(p) | 1))
+#define MICROPY_MAKE_POINTER_CALLABLE(p) ((void *)((mp_uint_t)(p) | 1))
 
 // This port is intended to be 32-bit, but unfortunately, int32_t for
 // different targets may be defined in different ways - either as int
@@ -63,10 +64,10 @@
 // to print such value. So, we avoid int32_t and use int directly.
 #define UINT_FMT "%u"
 #define INT_FMT "%d"
-typedef int mp_int_t; // must be pointer size
+typedef int mp_int_t;       // must be pointer size
 typedef unsigned mp_uint_t; // must be pointer size
 
-typedef void *machine_ptr_t; // must be of pointer size
+typedef void *machine_ptr_t;             // must be of pointer size
 typedef const void *machine_const_ptr_t; // must be of pointer size
 typedef long mp_off_t;
 
@@ -75,17 +76,17 @@ typedef long mp_off_t;
 // extra built in modules to add to the list of known ones
 extern const struct _mp_obj_module_t machine_module;
 
-#define MICROPY_PORT_BUILTIN_MODULES \
-    { MP_ROM_QSTR(MP_QSTR_umachine), MP_ROM_PTR(&machine_module) }, \
+#define MICROPY_PORT_BUILTIN_MODULES                                           \
+  {MP_ROM_QSTR(MP_QSTR_umachine), MP_ROM_PTR(&machine_module)},
 
 // extra constants
-#define MICROPY_PORT_CONSTANTS \
-    { MP_ROM_QSTR(MP_QSTR_umachine), MP_ROM_PTR(&machine_module) }, \
-    { MP_ROM_QSTR(MP_QSTR_machine), MP_ROM_PTR(&machine_module) }, \
+#define MICROPY_PORT_CONSTANTS                                                 \
+  {MP_ROM_QSTR(MP_QSTR_umachine), MP_ROM_PTR(&machine_module)},                \
+      {MP_ROM_QSTR(MP_QSTR_machine), MP_ROM_PTR(&machine_module)},
 
 // extra built in names to add to the global namespace
-#define MICROPY_PORT_BUILTINS \
-    { MP_ROM_QSTR(MP_QSTR_open), MP_ROM_PTR(&mp_builtin_open_obj) }, \
+#define MICROPY_PORT_BUILTINS                                                  \
+  {MP_ROM_QSTR(MP_QSTR_open), MP_ROM_PTR(&mp_builtin_open_obj)},
 
 // We have inlined IRQ functions for efficiency (they are generally
 // 1 machine instruction).
@@ -96,55 +97,53 @@ extern const struct _mp_obj_module_t machine_module;
 // to know the machine-specific values, see irq.h.
 
 static inline void enable_irq(mp_uint_t state) {
-    //__set_PRIMASK(state);
-    //#warning fix this, find the right include
+  //__set_PRIMASK(state);
+  //#warning fix this, find the right include
 }
 
 static inline mp_uint_t disable_irq(void) {
-    //#warning fix this, find the right include
-    //mp_uint_t state = __get_PRIMASK();
-    //__disable_irq();
-    mp_uint_t state = 0;
-    return state;
+  //#warning fix this, find the right include
+  // mp_uint_t state = __get_PRIMASK();
+  //__disable_irq();
+  mp_uint_t state = 0;
+  return state;
 }
 
-#define MICROPY_BEGIN_ATOMIC_SECTION()     disable_irq()
-#define MICROPY_END_ATOMIC_SECTION(state)  enable_irq(state)
+#define MICROPY_BEGIN_ATOMIC_SECTION() disable_irq()
+#define MICROPY_END_ATOMIC_SECTION(state) enable_irq(state)
 
 #if MICROPY_PY_THREAD
-#define MICROPY_EVENT_POLL_HOOK \
-    do { \
-        extern void mp_handle_pending(void); \
-        mp_handle_pending(); \
-        if (pyb_thread_enabled) { \
-            MP_THREAD_GIL_EXIT(); \
-            pyb_thread_yield(); \
-            MP_THREAD_GIL_ENTER(); \
-        } else { \
-            __WFI(); \
-        } \
-    } while (0);
+#define MICROPY_EVENT_POLL_HOOK                                                \
+  do {                                                                         \
+    extern void mp_handle_pending(void);                                       \
+    mp_handle_pending();                                                       \
+    if (pyb_thread_enabled) {                                                  \
+      MP_THREAD_GIL_EXIT();                                                    \
+      pyb_thread_yield();                                                      \
+      MP_THREAD_GIL_ENTER();                                                   \
+    } else {                                                                   \
+      __WFI();                                                                 \
+    }                                                                          \
+  } while (0);
 
 #define MICROPY_THREAD_YIELD() pyb_thread_yield()
 #else
-#define MICROPY_EVENT_POLL_HOOK \
-    do { \
-        extern void mp_handle_pending(void); \
-        mp_handle_pending(); \
-        __WFI(); \
-    } while (0);
+#define MICROPY_EVENT_POLL_HOOK                                                \
+  do {                                                                         \
+    extern void mp_handle_pending(void);                                       \
+    mp_handle_pending();                                                       \
+    __WFI();                                                                   \
+  } while (0);
 
 #define MICROPY_THREAD_YIELD()
 #endif
-
-
 
 // We need to provide a declaration/definition of alloca()
 #include <alloca.h>
 
 #define MP_STATE_PORT MP_STATE_VM
 
-#define MICROPY_PORT_ROOT_POINTERS \
-    const char *readline_hist[8]; \
-    mp_obj_t pin_class_mapper; \
-    mp_obj_t pin_class_map_dict;
+#define MICROPY_PORT_ROOT_POINTERS                                             \
+  const char *readline_hist[8];                                                \
+  mp_obj_t pin_class_mapper;                                                   \
+  mp_obj_t pin_class_map_dict;
