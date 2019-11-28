b5d913ca989739587262f0a831d722dea1fd6a63
oggi82 <57406801+oggi82@users.noreply.github.com>
2019-11-28 19:47:56 +0100

    ports/xmc: adding soft reboot print for pyboard.py.

--- ports/xmc/main.c
+++ ports/xmc/main.c
@@ -28,17 +28,16 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "lib/utils/pyexec.h"
 #include "py/compile.h"
-#include "py/runtime.h"
-#include "py/repl.h"
 #include "py/gc.h"
 #include "py/mperrno.h"
-#include "lib/utils/pyexec.h"
+#include "py/repl.h"
+#include "py/runtime.h"
 
-
-#include "py/stackctrl.h"
-#include "py/mphal.h"
 #include "lib/mp-readline/readline.h"
+#include "py/mphal.h"
+#include "py/stackctrl.h"
 // #include "lib/oofatfs/ff.h"
 // #include "extmod/vfs.h"
 //#include "extmod/vfs_fat.h"
@@ -50,25 +49,21 @@
 /* fSYS = 120MHz */
 /* fUSBPLL = 192MHz */
 /* fUSB = 48MHz */
-const XMC_SCU_CLOCK_CONFIG_t clock_config =
-{
-  .syspll_config.p_div = 2,
-  .syspll_config.n_div = 80,
-  .syspll_config.k_div = 4,
-  .syspll_config.mode = XMC_SCU_CLOCK_SYSPLL_MODE_NORMAL,
-  .syspll_config.clksrc = XMC_SCU_CLOCK_SYSPLLCLKSRC_OSCHP,
-  .enable_oschp = true,
-  .calibration_mode = XMC_SCU_CLOCK_FOFI_CALIBRATION_MODE_FACTORY,
-  .fsys_clksrc = XMC_SCU_CLOCK_SYSCLKSRC_PLL,
-  .fsys_clkdiv = 1,
-  .fcpu_clkdiv = 1,
-  .fccu_clkdiv = 1,
-  .fperipheral_clkdiv = 1
-};
+const XMC_SCU_CLOCK_CONFIG_t clock_config = {
+    .syspll_config.p_div = 2,
+    .syspll_config.n_div = 80,
+    .syspll_config.k_div = 4,
+    .syspll_config.mode = XMC_SCU_CLOCK_SYSPLL_MODE_NORMAL,
+    .syspll_config.clksrc = XMC_SCU_CLOCK_SYSPLLCLKSRC_OSCHP,
+    .enable_oschp = true,
+    .calibration_mode = XMC_SCU_CLOCK_FOFI_CALIBRATION_MODE_FACTORY,
+    .fsys_clksrc = XMC_SCU_CLOCK_SYSCLKSRC_PLL,
+    .fsys_clkdiv = 1,
+    .fcpu_clkdiv = 1,
+    .fccu_clkdiv = 1,
+    .fperipheral_clkdiv = 1};
 
-
-void SystemCoreClockSetup(void)
-{
+void SystemCoreClockSetup(void) {
   /* Setup settings for USB clock */
   XMC_SCU_CLOCK_Init(&clock_config);
 
@@ -81,114 +76,120 @@ void SystemCoreClockSetup(void)
   SystemCoreClockUpdate();
 }
 
-
 #if MICROPY_ENABLE_COMPILER
 void do_str(const char *src, mp_parse_input_kind_t input_kind) {
-    nlr_buf_t nlr;
-    if (nlr_push(&nlr) == 0) {
-        mp_lexer_t *lex = mp_lexer_new_from_str_len(MP_QSTR__lt_stdin_gt_, src, strlen(src), 0);
-        qstr source_name = lex->source_name;
-        mp_parse_tree_t parse_tree = mp_parse(lex, input_kind);
-        mp_obj_t module_fun = mp_compile(&parse_tree, source_name, true);
-        mp_call_function_0(module_fun);
-        nlr_pop();
-    } else {
-        // uncaught exception
-        mp_obj_print_exception(&mp_plat_print, (mp_obj_t)nlr.ret_val);
-    }
+  nlr_buf_t nlr;
+  if (nlr_push(&nlr) == 0) {
+    mp_lexer_t *lex =
+        mp_lexer_new_from_str_len(MP_QSTR__lt_stdin_gt_, src, strlen(src), 0);
+    qstr source_name = lex->source_name;
+    mp_parse_tree_t parse_tree = mp_parse(lex, input_kind);
+    mp_obj_t module_fun = mp_compile(&parse_tree, source_name, true);
+    mp_call_function_0(module_fun);
+    nlr_pop();
+  } else {
+    // uncaught exception
+    mp_obj_print_exception(&mp_plat_print, (mp_obj_t)nlr.ret_val);
+  }
 }
 #endif
 
 static char *stack_top;
 #if MICROPY_ENABLE_GC
-static char heap[2048];
+static char heap[16384];
 #endif
 
 int main(int argc, char **argv) {
-    int stack_dummy;
-    stack_top = (char*)&stack_dummy;
-    USB_Init();
+  int stack_dummy;
+  stack_top = (char *)&stack_dummy;
+  USB_Init();
 soft_reset:
-    #if MICROPY_ENABLE_GC
-    gc_init(heap, heap + sizeof(heap));
-    #endif
+#if MICROPY_ENABLE_GC
+  gc_init(heap, heap + sizeof(heap));
+#endif
 
-    readline_init0();
-    pin_init0();
-    mp_init();
-    #if MICROPY_ENABLE_COMPILER
-    #if MICROPY_REPL_EVENT_DRIVEN
-    pyexec_event_repl_init();
-    for (;;) {
-        int c = mp_hal_stdin_rx_chr();
-        if (pyexec_event_repl_process_char(c)) {
-            break;
-        }
+  readline_init0();
+  pin_init0();
+  mp_init();
+#if MICROPY_ENABLE_COMPILER
+#if MICROPY_REPL_EVENT_DRIVEN
+  pyexec_event_repl_init();
+  for (;;) {
+    int c = mp_hal_stdin_rx_chr();
+    if (pyexec_event_repl_process_char(c)) {
+      break;
     }
-    #else
-        #if MICROPY_ENABLE_COMPILER
-        // Main script is finished, so now go into REPL mode.
-        // The REPL mode can change, or it can request a soft reset.
-        for (;;) {
-            if (pyexec_mode_kind == PYEXEC_MODE_RAW_REPL) {
-                if (pyexec_raw_repl() != 0) {
-                    break;
-                }
-            } else {
-                if (pyexec_friendly_repl() != 0) {
-                    break;
-                }
-            }
-        }
-        #endif 
-    #endif
-    //do_str("print('hello world!', list(x+1 for x in range(10)), end='eol\\n')", MP_PARSE_SINGLE_INPUT);
-    //do_str("for i in range(10):\r\n  print(i)", MP_PARSE_FILE_INPUT);
-    #else
-    pyexec_frozen_module("frozentest.py");
-    #endif
-soft_reset_exit:
-    printf("MPY: soft reboot\n");
-    mp_deinit();
-    gc_sweep_all();
-    goto soft_reset;
-    return 0;
+  }
+#else
+#if MICROPY_ENABLE_COMPILER
+  // Main script is finished, so now go into REPL mode.
+  // The REPL mode can change, or it can request a soft reset.
+  for (;;) {
+    if (pyexec_mode_kind == PYEXEC_MODE_RAW_REPL) {
+      if (pyexec_raw_repl() != 0) {
+        break;
+      }
+    } else {
+      if (pyexec_friendly_repl() != 0) {
+        break;
+      }
+    }
+  }
+#endif
+#endif
+// do_str("print('hello world!', list(x+1 for x in range(10)), end='eol\\n')",
+// MP_PARSE_SINGLE_INPUT); do_str("for i in range(10):\r\n  print(i)",
+// MP_PARSE_FILE_INPUT);
+#else
+  pyexec_frozen_module("frozentest.py");
+#endif
+  // soft_reset_exit:
+  printf("MPY: soft reboot\n");
+  mp_deinit();
+  gc_sweep_all();
+  goto soft_reset;
+  return 0;
 }
 
 void gc_collect(void) {
-    // WARNING: This gc_collect implementation doesn't try to get root
-    // pointers from CPU registers, and thus may function incorrectly.
-    void *dummy;
-    gc_collect_start();
-    gc_collect_root(&dummy, ((mp_uint_t)stack_top - (mp_uint_t)&dummy) / sizeof(mp_uint_t));
-    gc_collect_end();
-    gc_dump_info();
+  // WARNING: This gc_collect implementation doesn't try to get root
+  // pointers from CPU registers, and thus may function incorrectly.
+  void *dummy;
+  gc_collect_start();
+  gc_collect_root(&dummy, ((mp_uint_t)stack_top - (mp_uint_t)&dummy) /
+                              sizeof(mp_uint_t));
+  gc_collect_end();
+  gc_dump_info();
 }
 
 mp_lexer_t *mp_lexer_new_from_file(const char *filename) {
-    mp_raise_OSError(MP_ENOENT);
+  mp_raise_OSError(MP_ENOENT);
 }
 
 mp_import_stat_t mp_import_stat(const char *path) {
-    return MP_IMPORT_STAT_NO_EXIST;
+  return MP_IMPORT_STAT_NO_EXIST;
 }
 
-mp_obj_t mp_builtin_open(size_t n_args, const mp_obj_t *args, mp_map_t *kwargs) {
-    return mp_const_none;
+mp_obj_t mp_builtin_open(size_t n_args, const mp_obj_t *args,
+                         mp_map_t *kwargs) {
+  return mp_const_none;
 }
 MP_DEFINE_CONST_FUN_OBJ_KW(mp_builtin_open_obj, 1, mp_builtin_open);
 
 void nlr_jump_fail(void *val) {
-    while (1);
+  while (1)
+    ;
 }
 
 void NORETURN __fatal_error(const char *msg) {
-    while (1);
+  while (1)
+    ;
 }
 
 #ifndef NDEBUG
-void MP_WEAK __assert_func(const char *file, int line, const char *func, const char *expr) {
-    printf("Assertion '%s' failed, at file %s:%d\n", expr, file, line);
-    __fatal_error("Assertion failed");
+void MP_WEAK __assert_func(const char *file, int line, const char *func,
+                           const char *expr) {
+  printf("Assertion '%s' failed, at file %s:%d\n", expr, file, line);
+  __fatal_error("Assertion failed");
 }
 #endif
