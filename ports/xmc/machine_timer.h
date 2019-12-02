47bdec374a984627c64e42d5b60fc7486b758204
oggi82 <57406801+oggi82@users.noreply.github.com>
2019-12-02 22:14:23 +0100

    ports/xmc: adding skeleton implementation for timer/CCU4 interface.

---
+++ ports/xmc/machine_timer.h
@@ -0,0 +1,44 @@
+/*
+ * This file is part of the MicroPython project, http://micropython.org/
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2013, 2014 Damien P. George
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#ifndef MICROPY_INCLUDED_XMC_MACHINE_TIMER_H
+#define MICROPY_INCLUDED_XMC_MACHINE_TIMER_H
+
+#include "xmc_ccu4.h"
+
+
+
+extern const mp_obj_type_t machine_timer_type;
+
+void timer_init0(void);
+void timer_tim5_init(void);
+XMC_CCU4_SLICE_t *timer_tim6_init(uint freq);
+void timer_deinit(void);
+uint32_t timer_get_source_freq(uint32_t tim_id);
+void timer_irq_handler(uint tim_id);
+
+XMC_CCU4_SLICE_t *machine_timer_get_handle(mp_obj_t timer);
+
+#endif // MICROPY_INCLUDED_XMC_MACHINE_TIMER_H
