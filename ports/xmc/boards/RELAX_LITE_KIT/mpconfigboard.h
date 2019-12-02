ef6a2340ca1967090b10f4fcdd1a372beab94141
oggi82 <57406801+oggi82@users.noreply.github.com>
2019-12-02 22:12:15 +0100

    ports/mxc: Add max available timer, maybe review needed.

--- ports/xmc/boards/RELAX_LITE_KIT/mpconfigboard.h
+++ ports/xmc/boards/RELAX_LITE_KIT/mpconfigboard.h
@@ -1,6 +1,7 @@
 #define MICROPY_HW_BOARD_NAME       "Relax Lite Kit-V1"
 #define MICROPY_HW_MCU_NAME         "XMC4500-1024"
 
+#define MICROPY_HW_MAX_TIMER        (16)
 #define MICROPY_HW_ENABLE_DAC       (0)
 #define MICROPY_HW_ENABLE_USB       (0)
 
